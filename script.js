d270fa6422ffc6af5652f22182b668db3d8e95b7
matheus-ma1a <matheussantosmaia@gmail.com>
2021-10-07 15:55:34 -0300

    mostra o numero dos caracteres digitados na tela

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// console.log(input.value.length);
+
 const input = document.querySelector(".input")
+
 const numbers = document.querySelector(".numbers")
-input.addEventListener("keydown", (keydown) => {
-    numbers.innerHTML = input.value.length + 1
-    console.log(input.value.length + 1)
+
+
+input.addEventListener("input", (keydown) => {
+    
+    numbers.innerHTML = input.value.length
     
 })
+
+
+
